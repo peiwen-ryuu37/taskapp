9b00f5a6c97a56f36843d0f745263815968c7e0d
peiwen.ryuu <liu@i-enter.co.jp>
2021-04-15 14:48:02 +0900

    タスクの追加、削除機能完成

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -33,13 +33,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let inputViewController: InputViewController = segue.destination as! InputViewController
         
-        is segue.identifier == "cellSegue" {
+        if segue.identifier == "cellSegue" {
             let indexPath = self.tableView.indexPathForSelectedRow
             inputViewController.task = taskArray[indexPath!.row]
         } else {
             let task = Task()
             
-            let allTasks = realm.object(Task.self)
+            let allTasks = realm.objects(Task.self)
             if allTasks.count != 0 {
                 task.id = allTasks.max(ofProperty: "id")! + 1
             }
@@ -90,11 +90,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //Deleteボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            
+            //削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            
+            //ローカル通知をキャンセルする
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            
             //データベースから削除する
             try! realm.write {
                 self.realm.delete(self.taskArray[indexPath.row])
                 tableView.deleteRows(at: [indexPath], with: .fade)
             }
+            
+            //未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                for request in requests {
+                    print("/----------")
+                    print(request)
+                    print("----------/")
+                }
+            }
+            
         }
     }
 
